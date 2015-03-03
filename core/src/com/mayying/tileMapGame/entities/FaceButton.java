53f2b21058b4791d1ab8b5e99a2ba0e4273f31f5
Htoowaiyan <htoowaiyan94@gmail.com>
2015-03-03 10:54:50 +0800

    waiyan

---
+++ core/src/com/mayying/tileMapGame/entities/FaceButton.java
@@ -0,0 +1,13 @@
+package com.mayying.tileMapGame.entities;
+
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+/**
+ * Created by HtooWaiYan on 3/1/2015.
+ */
+public class FaceButton extends TextButton {
+
+    public FaceButton(String text, TextButtonStyle style) {
+        super(text, style);
+    }
+}
